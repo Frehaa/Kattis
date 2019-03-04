6e3d65be012fcc90ab9e40ec70246aec2819f339
Frederik Haagensen <haag@itu.dk>
2019-03-04 10:21:20 +0100

    Haskell number types are wierd

--- yoda.hs
+++ yoda.hs
@@ -1,6 +1,12 @@
 digit_n x n = (x `mod` 10^n) `div` 10^(n-1)
 
+log10 = logBase $ fromIntegral 10
+num_length num = ceiling $ log10 $ fromIntegral num 
+
 main = do
     a <- readLn :: IO Int
     b <- readLn :: IO Int 
-    print ("test")
+    let len_a = num_length a
+    let len_b = num_length b
+    let length = max len_a len_b
+    print length 
