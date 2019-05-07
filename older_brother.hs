85d601233edb0f06fdf35de21b85d9e0c047b533
haag <haag@itu.dk>
2019-05-07 11:10:08 +0200

    Modified older brother

--- older_brother.hs
+++ older_brother.hs
@@ -15,11 +15,17 @@ binary p q lo hi
     where mid = lo + (hi-lo) `div` 2 
           t = p ^ mid
 
+search p q i
+    | q' == q = Just i
+    | q' <  q = search p q (i + 1)
+    | q' >  q = Nothing
+    where q' = p ^ i    
+
 main = do 
     q <- readLn :: IO Int
     let ma = highest_prime q
     let nums = [2..ma] ++ (if q > ma then [q] else [])
     let primes = sieve nums
-    if any (\p -> binary p q 1 30 /= Nothing) primes 
+    if any (\p -> search p q 1 /= Nothing) primes 
         then putStrLn "yes" 
         else putStrLn "no" 
