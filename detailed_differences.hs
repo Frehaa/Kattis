f6923264c3842d6884d63c15e4f6a5bcd0d62ad4
frehaa <frehaa@hotmail.dk>
2019-08-20 15:11:09 +0200

    Finish detailed differences

--- detailed_differences.hs
+++ detailed_differences.hs
@@ -1,13 +1,15 @@
+import Control.Monad
 
-getInt :: IO Int
-getInt = fmap read getLine
-
-main = do 
-    n <- getInt
-    print n
+doStuff :: IO ()
+doStuff = do
     l1 <- getLine
     l2 <- getLine
 
+    putStrLn l1
+    putStrLn l2
+    putStrLn $ zipWith (\x y -> if x == y then '.' else '*') l1 l2
+    putStrLn ""
 
-
-    print (l1 ++ l2)
+main = do 
+    n <- fmap read getLine
+    replicateM n doStuff
