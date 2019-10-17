fd12d58a66001735ec221390f35a62591ff63bf3
frehaa <frehaa@hotmail.dk>
2019-10-17 12:39:32 +0200

    Update knapsack test

--- mk_knapsack_test.py
+++ mk_knapsack_test.py
@@ -3,9 +3,10 @@ import random as r
 c = int(input())
 n = int(input())
 
-print c, n
+for _ in range(30):
+    print c, n
 
-for _ in range(n):
-    v = r.randint(1, 10000)
-    w = r.randint(1, 10000)
-    print v, w
+    for _ in range(n):
+        v = r.randint(1, 10000)
+        w = r.randint(1, 10000)
+        print v, w
